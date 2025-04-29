27c2fac7d5e5cc3e9eb8b67f8b013c94a9184b8e
Errol Lin <74441963+errollin@users.noreply.github.com>
2025-04-29 16:14:34 +0800

    Update data initializer

--- src/main/java/com/errol/springbootweb/component/DataInitializer.java
+++ src/main/java/com/errol/springbootweb/component/DataInitializer.java
@@ -2,8 +2,10 @@ package com.errol.springbootweb.component;
 
 import com.errol.springbootweb.entity.Author;
 import com.errol.springbootweb.entity.Book;
+import com.errol.springbootweb.entity.Publisher;
 import com.errol.springbootweb.repository.AuthorRepository;
 import com.errol.springbootweb.repository.BookRepository;
+import com.errol.springbootweb.repository.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class DataInitializer implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public DataInitializer(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DataInitializer(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -45,9 +49,26 @@ public class DataInitializer implements CommandLineRunner {
 
         savedErrol.getBooks().add(savedDddBook);
         savedViolet.getBooks().add(savedSeBook);
+        savedDddBook.getAuthors().add(savedErrol);
+        savedSeBook.getAuthors().add(savedViolet);
+
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("Errol's Publisher");
+        publisher.setAddress("ShangHai");
+        Publisher savedPublisher = this.publisherRepository.save(publisher);
+
+        savedDddBook.setPublisher(savedPublisher);
+        savedSeBook.setPublisher(savedPublisher);
+
+        this.authorRepository.save(savedErrol);
+        this.authorRepository.save(savedViolet);
+        this.bookRepository.save(savedDddBook);
+        this.bookRepository.save(savedSeBook);
 
         System.out.println("Initialize data: ");
         System.out.println("Author Count: " + this.authorRepository.count());
         System.out.println("Book Count: " + this.bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
     }
 }
